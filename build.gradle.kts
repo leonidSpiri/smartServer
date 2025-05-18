50e860035249c075ddbb99d73898a1fc6505c802
leonidSpiri <2003.leonid2003@gmail.com>
2025-05-18 12:27:58 +0300

    update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ group = "ru.spiridonov"
 version = "0.0.1"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
@@ -47,7 +47,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = "17"
+        jvmTarget = "21"
     }
 }
 
